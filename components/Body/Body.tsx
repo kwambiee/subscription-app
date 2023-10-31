8cd9b75eda311552e2dfbf1bf89e82b7c0ecadf1
IainMosima <iainmosima@gmail.com>
2023-10-31 12:48:07 +0300

    added navbar and created card component

---
+++ components/Body/Body.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React from 'react';
+import "./Body.scss";
+
+const cards = [
+    {
+        
+    }
+]
+const Body = () => {
+    return (
+        <div className='min-h-screen flex flex-col gap-[6rem] place-items-center justify-center'>
+            <h2 className='text-[3rem] font-semibold italic'>Discover a plan tailored to your specific requirements</h2>
+
+           
+
+        </div>
+    )
+}
+
+export default Body
